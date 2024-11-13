7573e86dc6ca8f0f49c71812c28b5d2689216cc1
Matts52 <32623708+Matts52@users.noreply.github.com>
2024-11-12 21:15:09 -0600

    add snowflake tversky

--- macros/tversky_coef.sql
+++ macros/tversky_coef.sql
@@ -57,3 +57,65 @@
 
 {% endmacro %}
 
+{% macro snowflake__tversky_coef(column_one, column_two, alpha, beta) %}
+
+    -- numerator query: intersection of two sets
+    {% set numerator_query %}
+        (
+            SELECT 
+                COUNT(DISTINCT value) AS intersection_count
+            FROM (
+                SELECT VALUE
+                FROM TABLE(FLATTEN(INPUT => {{ column_one }})) AS a
+                INTERSECT
+                SELECT VALUE
+                FROM TABLE(FLATTEN(INPUT => {{ column_two }})) AS b
+            ) AS intersection
+        )::float
+    {% endset %}
+
+    -- denominator query: weighted Tversky formula
+    {% set denominator_query %}
+        (
+            -- intersection
+            SELECT COUNT(DISTINCT value) 
+            FROM (
+                SELECT VALUE
+                FROM TABLE(FLATTEN(INPUT => {{ column_one }})) AS a
+                INTERSECT
+                SELECT VALUE
+                FROM TABLE(FLATTEN(INPUT => {{ column_two }})) AS b
+            ) AS intersection
+        )::float
+
+        +
+
+        -- elements in column_one only (A - B)
+        (SELECT {{ alpha }} * COUNT(DISTINCT value)
+         FROM (
+             SELECT VALUE
+             FROM TABLE(FLATTEN(INPUT => {{ column_one }})) AS a
+             MINUS
+             SELECT VALUE
+             FROM TABLE(FLATTEN(INPUT => {{ column_two }})) AS b
+         ) AS only_in_a
+        )::float
+
+        +
+
+        -- elements in column_two only (B - A)
+        (SELECT {{ beta }} * COUNT(DISTINCT value)
+         FROM (
+             SELECT VALUE
+             FROM TABLE(FLATTEN(INPUT => {{ column_two }})) AS b
+             MINUS
+             SELECT VALUE
+             FROM TABLE(FLATTEN(INPUT => {{ column_one }})) AS a
+         ) AS only_in_b
+        )::float
+    {% endset %}
+
+    -- safe divide the two
+    {{ dbt_utils.safe_divide(numerator_query, denominator_query) }}
+
+{% endmacro %}
