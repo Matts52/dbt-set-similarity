1ff7f7a532c44ab05a392d3822f498808fd3d301
Matts52 <32623708+Matts52@users.noreply.github.com>
2024-12-28 18:28:27 -0600

    get dice function to pass

--- integration_tests/models/test_jaccard_coef.sql
+++ integration_tests/models/test_jaccard_coef.sql
@@ -1,8 +1,17 @@
 with data_jaccard_coef as (
     select
-        input_one::int[] AS input_one,
-        input_two::int[] AS input_two,
-        output
+        {% if adapter == 'postgres' %}
+            input_one::int[] as input_one,
+            input_two::int[] as input_two,
+        {% elif adapter == 'duckdb' %}
+            str_split(trim(both '{}' from input_one), ',') as input_one,
+            str_split(trim(both '{}' from input_two), ',') as input_two,
+        {% else %}
+            -- Default behavior
+            input_one,
+            input_two,
+        {% endif %}
+            output
     from {{ ref('data_jaccard_coef') }}
 )
 
