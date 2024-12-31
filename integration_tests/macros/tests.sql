34242b679f9af5020c817335474d65cbf0849046
Matts52 <32623708+Matts52@users.noreply.github.com>
2024-12-31 13:55:19 -0600

    update test to reflect jinja parameters

--- integration_tests/macros/tests.sql
+++ integration_tests/macros/tests.sql
@@ -1,5 +1,5 @@
 {% test assert_equal(model, actual, expected) %}
     select * from {{ model }}
     where
-        abs(actual - expected) > 0.0001
+        abs({{ actual }} - {{ expected }}) > 0.0001
 {% endtest %}
